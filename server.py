ee7cdfb3a00b9a25ecf05843a72a35fca68ca389
Grebtsew <danwe980@student.liu.se>
2020-03-04 21:29:14 +0100

    Update server.py

--- server.py
+++ server.py
@@ -5,6 +5,7 @@ from functools import partial
 
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
+
 class S(BaseHTTPRequestHandler):
     CRYPT = "password-1"
     connection_list = []
@@ -16,7 +17,8 @@ class S(BaseHTTPRequestHandler):
     def _set_response(self):
         self.send_response(200, "ok")
         self.send_header('Access-Control-Allow-Origin', '*')
-        self.send_header('Access-Control-Allow-Methods', 'POST, OPTIONS, HEAD, GET')
+        self.send_header('Access-Control-Allow-Methods',
+                         'POST, OPTIONS, HEAD, GET')
         self.send_header("Access-Control-Allow-Headers", "X-Requested-With")
         self.send_header("Access-Control-Allow-Headers", "Content-Type")
         self.send_header('Content-type', 'text/html')
@@ -29,33 +31,39 @@ class S(BaseHTTPRequestHandler):
         self._set_response()
 
     def do_POST(self):
-        #print(self.client_address,self.headers)
+        # print(self.client_address,self.headers)
 
         if self.headers['Content-Length']:
 
-            content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
-            post_data = self.rfile.read(content_length) # <--- Gets the data itself
-            #print(post_data)
+            # <--- Gets the size of data
+            content_length = int(self.headers['Content-Length'])
+            # <--- Gets the data itself
+            post_data = self.rfile.read(content_length)
+            # print(post_data)
             # decode incoming data // see if password is correct here!
-            if post_data.decode('utf-8').__contains__("api_crypt="+self.CRYPT):
-                data = post_data.decode('utf-8').split("&")
+            try:
 
-                # detemine source data
-                if not self.client_address[0] in self.connection_list:
-                    self.speaker.say("New Server Connection From Address " + str(self.client_address[0]) + " PORT " + str(self.client_address[1]) )
-                    self.connection_list.append(self.client_address[0])
+                data = json.loads(post_data.decode('utf-8'))
+                #print("json", data)
+                if data['api_crypt']:
+                    if data['api_crypt'] == self.CRYPT:
 
-                for d in data:
-                    v = d.replace("+"," ")
-                    value = v.split("=")
+                        # detemine source data
+                        if not self.client_address[0] in self.connection_list:
+                            self.speaker.say("New Server Connection From Address " + str(
+                                self.client_address[0]) + " PORT " + str(self.client_address[1]))
+                            self.connection_list.append(self.client_address[0])
 
-                    if value[0] == "api_rate":
-                        self.speaker.setSpeed(float(value[1]))
-                    elif value[0] == "api_text":
-                        self.speaker.say(value[1])
-                    elif value[0] == "api_volume":
-                        self.speaker.setVolume(float(value[1]))
+                        if data["api_rate"]:
+                            self.speaker.setSpeed(float(data["api_rate"]))
+                        elif data["api_text"]:
+                            self.speaker.say(data["api_text"])
+                        elif data["api_volume"]:
+                            self.speaker.setVolume(float(data["api_volume"]))
+            except Exception:
+                pass
         self._set_response()
+
 
 class server(Thread):
 
@@ -67,7 +75,7 @@ class server(Thread):
 
     def run(self):
         server_address = ('localhost', self.PORT)
-        httpd = HTTPServer(server_address, partial( S, self.speaker))
+        httpd = HTTPServer(server_address, partial(S, self.speaker))
         try:
             httpd.serve_forever()
         except KeyboardInterrupt:
